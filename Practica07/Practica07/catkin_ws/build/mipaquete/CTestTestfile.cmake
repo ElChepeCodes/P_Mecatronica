4c16a36bf3e1172d08d6a7bd3f36b65cf1779315
Jose Luis Gutierrez Espinosa <josepe430@gmail.com>
2022-11-04 16:35:44 -0600

    p7

---
+++ Practica07/Practica07/catkin_ws/build/mipaquete/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/chepe/catkin_ws/src/mipaquete
+# Build directory: /home/chepe/catkin_ws/build/mipaquete
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
